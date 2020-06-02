7349ea7d4ad48fbbded82bf2152d03f6d0f03904
Jonathan Beardstone <jb@ouetquand.xyz>
2020-06-02 18:21:59 +0200

    fixing some issues

--- components/FvPartnerStepDetail/index.vue
+++ components/FvPartnerStepDetail/index.vue
@@ -2,19 +2,45 @@
   .fv-partner-step-detail
     v-row
       v-col(cols="6")
-        span plop
+        v-text-field(
+          label="$t('model.partner.name')"
+          outlined=''
+          clearable=''
+        )
       v-col(cols="6")
-        span {{ formesJuridiques }}
+        v-select(
+          :items="$store.getters['formesJuridiques']"
+          item-value="id"
+          item-text="name"
+          outlined=''
+          clearable=''
+        )
     v-row
       v-col(cols="6")
-        span azeaze
+        v-text-field(
+          label="$t('model.partner.address')"
+          outlined=''
+          clearable=''
+        )
       v-col(cols="6")
-        span azeaze
+        v-text-field(
+          label="$t('model.partner.addressmore')"
+          outlined=''
+          clearable=''
+        )
     v-row
       v-col(cols="6")
-        span azeaze
+        v-text-field(
+          label="$t('model.partner.zip')"
+          outlined=''
+          clearable=''
+        )
       v-col(cols="6")
-        span azeaze
+        v-text-field(
+          label="$t('model.partner.city')"
+          outlined=''
+          clearable=''
+        )
 </template>
 
 <script>
