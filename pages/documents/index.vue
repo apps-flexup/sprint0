7f1868f5cf7f8bd1b4f14fb5796d51e728c0f5eb
gmevelec <gregory.mevelec@gmail.com>
2020-06-18 18:42:55 +0200

    Add page documents

---
+++ pages/documents/index.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  .documents
+</template>
+
+<script>
+export default {
+  computed: {
+    machin() {
+      return 42
+    }
+  },
+  mounted() {
+    console.log('Composant ', this.$options.name)
+  }
+}
+</script>
+<style lang="scss" scoped>
+.documents {
+  background-color: inherit;
+}
+</style>
