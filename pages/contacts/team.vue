855e816bb3ef7c625c94f632bbee1cf5083520c5
Jonathan Beardstone <jb@ouetquand.xyz>
2020-07-07 14:14:42 +0200

    Add team page and contact card

---
+++ pages/contacts/team.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  .team-list.d-flex.flex-wrap.align-content-space-around
+    fv-contact-card.align-self-stretch.ma-2(
+      v-for="item in items"
+      :key="item.id"
+      :contact="item"
+    )
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    }
+  },
+  mounted() {
+    console.log('Composant ', this.$options.name)
+    this.$repos.team.index().then((res) => {
+      this.items = res
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+.team-list {
+  max-width: 900px;
+  // height: 500px;
+}
+</style>
