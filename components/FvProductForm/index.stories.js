4bf9fef7625734773edb449aa54cf9aa7740905a
gmevelec <gregory.mevelec@gmail.com>
2020-07-29 16:43:49 +0200

    Add product form story

---
+++ components/FvProductForm/index.stories.js
@@ -0,0 +1,25 @@
+import { storiesOf } from '@storybook/vue'
+import FvProductForm from './index.vue'
+
+storiesOf('Molecules|FvProductForm', module).add(
+  'Common',
+  () => ({
+    components: { FvProductForm },
+    data() {
+      return {
+        product: {}
+      }
+    },
+    methods: {
+      productChanged(_v) {}
+    },
+    template: `<FvProductForm
+      :product="product"
+      @product:changed="productChanged"
+    >
+    </FvProductForm>`
+  }),
+  {
+    info: {}
+  }
+)
