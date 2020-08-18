00fa63c5cf043fe6ac3d8df93caaeb97b7e60468
antoine <anto.puech@gmail.com>
2020-08-18 16:29:46 +0200

    add logoutButton story

---
+++ components/FvLogoutButton/index.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  v-btn(
+    @click.stop="click"
+    color="primary"
+  ) {{ $t('buttons.logout') }}
+</template>
+
+<script>
+export default {
+  name: 'FvLogoutButton',
+  mounted() {
+    console.log('Composant ', this.$options.name)
+  },
+  methods: {
+    click() {
+      this.$emit('logoutButton:clicked')
+    }
+  }
+}
+</script>
