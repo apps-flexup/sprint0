b2be117497473810cd3cdb2b2f0cb358e1bc1b16
antoine <anto.puech@gmail.com>
2020-08-19 18:06:38 +0200

    add account storybook and navBar

---
+++ components/FvAccount/index.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+.fv-account
+  v-list-item
+    fv-avatar(
+      :avatar="avatar"
+    )
+    v-list-item-content
+      v-list-item-title {{ name }}
+</template>
+
+<script>
+export default {
+  name: 'FvAccount',
+  props: {
+    name: {
+      type: String,
+      default() {
+        return 'Name'
+      }
+    },
+    avatar: {
+      type: String,
+      default() {
+        return ''
+      }
+    }
+  },
+  mounted() {
+    console.log('Composant ', this.$options.name)
+  }
+}
+</script>
