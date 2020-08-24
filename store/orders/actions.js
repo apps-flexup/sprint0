e13f45dad927e2d9d7f48bf76697dd967c57afba
gmevelec <gregory.mevelec@gmail.com>
2020-08-24 19:09:58 +0200

    Add add action to order store

--- store/orders/actions.js
+++ store/orders/actions.js
@@ -3,5 +3,8 @@ export default {
   get({ commit }) {
     // charger les contrat-types
     this.$repos.orders.indexWithAccountId().then((data) => commit('set', data))
+  },
+  add({ commit }, order) {
+    console.log('Order to add: ', order)
   }
 }
