c804f54f9066223625b3986d33f39d99db3a529a
antoine <anto.puech@gmail.com>
2020-08-27 14:20:25 +0200

    add list of icon component and storybook

--- components/FvIcon/index.stories.js
+++ components/FvIcon/index.stories.js
@@ -12,6 +12,7 @@ const icon = {
   bluetooth: 'mdi-bluetooth'
 }
 const defaultIcon = 'mdi-plus'
+const iconsNames = Object.keys(icon)
 const color = {
   black: '#000000',
   red: 'red',
@@ -32,27 +33,47 @@ const size = {
 }
 const defaultSize = 'medium'
 
-storiesOf('Atoms/FvIcon', module).add('Common', () => ({
-  props: {
-    icon: {
-      default: select('Icon', icon, defaultIcon, 'Props')
+storiesOf('Atoms/FvIcon', module)
+  .add('Common', () => ({
+    props: {
+      icon: {
+        default: select('Icon', icon, defaultIcon, 'Props')
+      },
+      color: {
+        default: select('Color', color, defaultColor, 'Props')
+      },
+      size: {
+        default: select('Size', size, defaultSize, 'Props')
+      }
     },
-    color: {
-      default: select('Color', color, defaultColor, 'Props')
+    components: { FvIcon },
+    methods: {
+      clicked: action('click')
     },
-    size: {
-      default: select('Size', size, defaultSize, 'Props')
-    }
-  },
-  components: { FvIcon },
-  methods: {
-    clicked: action('click')
-  },
-  template: `<FvIcon
-    :icon="icon"
-    :color="color"
-    :size="size"
-    @icon:clicked="clicked"
-  >
-  </FvIcon>`
-}))
+    template: `<FvIcon
+      :icon="icon"
+      :color="color"
+      :size="size"
+      @icon:clicked="clicked"
+    >
+    </FvIcon>`
+  }))
+  .add('Available icons', () => ({
+    components: { FvIcon },
+    data() {
+      return {
+        iconsNames,
+        icon
+      }
+    },
+    template: `<div :style="{display: 'flex', flexWrap: 'wrap'}">
+      <div
+          v-for="icons in icon"
+          :key="icon"
+          :style="{display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '.5rem',flex: '0 0 50px', height: '100px', font: '300 12px Raleway'}">
+        <FvIcon :icon="icons" :style="{margin: '0 0 8px 0'}"/>
+      <div
+        <span :style="{textOverflow: 'ellipsis', whiteSpace: 'nowrap', width: '60px', textAlign: 'center'}">{{ icons.slice(4) }}</span>
+      </div>
+    </div>`
+  }))
