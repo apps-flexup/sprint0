9a902ffa797790c130575fac13c1d8b8c90dfff2
gmevelec <gregory.mevelec@gmail.com>
2020-08-28 13:52:47 +0200

    Mdx mdx again

--- components/FvMenuList/index.stories.js
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from '@storybook/vue'
-import FvMenuList from './index.vue'
-
-storiesOf('Molecules/FvMenuList', module).add('Common', () => ({
-  components: { FvMenuList },
-  template: `<FvMenuList>
-  </FvMenuList>`
-}))
