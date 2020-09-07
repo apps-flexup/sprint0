fc46ee9a40a629b0dad56003f83ae27067556f6e
antoine <anto.puech@gmail.com>
2020-09-07 16:50:41 +0200

    tmp commit

---
+++ .storybook/knobs_data/colorsAvailable.js
@@ -0,0 +1,10 @@
+export const colors = {
+  black: '#000000',
+  red: 'red',
+  blue: 'blue',
+  green: 'green',
+  yellow: 'yellow',
+  purple: 'purple',
+  white: '#ffffff'
+}
+export const defaultColor = '#000000'
---
+++ .storybook/knobs_data/sizesAvailable.js
@@ -0,0 +1,8 @@
+export const sizes = {
+  xSmall: 'xSmall',
+  small: 'small',
+  medium: 'medium',
+  large: 'large',
+  xLarge: 'xLarge'
+}
+export const defaultSize = 'medium'
