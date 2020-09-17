736f0d7dba820470202ba11ae9637148374d887c
gmevelec <gregory.mevelec@gmail.com>
2020-09-17 18:30:46 +0200

    Add vat field component

---
+++ components/FvVatField/index.vue
@@ -0,0 +1,69 @@
+<template lang="pug">
+.fv-vat-field
+  fv-number-field(
+    :value="vat"
+    :label="label"
+    :outlined="outlined"
+    :readonly="readonly"
+    :clearable="clearable"
+    suffix="%"
+    @input="vatChanged"
+  )
+</template>
+
+<script>
+export default {
+  name: 'FvPriceField',
+  props: {
+    value: {
+      type: String,
+      default() {
+        return ''
+      }
+    },
+    label: {
+      type: String,
+      default() {
+        return ''
+      }
+    },
+    outlined: {
+      type: Boolean,
+      default() {
+        return true
+      }
+    },
+    readonly: {
+      type: Boolean,
+      default() {
+        return false
+      }
+    },
+    clearable: {
+      type: Boolean,
+      default() {
+        return true
+      }
+    }
+  },
+  data() {
+    return {
+      vat: this.value
+    }
+  },
+  watch: {
+    value() {
+      this.vat = this.value
+    }
+  },
+  mounted() {
+    console.log('Composant', this.$options.name)
+  },
+  methods: {
+    vatChanged(v) {
+      this.vat = v
+      this.$emit('vat:changed', this.vat)
+    }
+  }
+}
+</script>
