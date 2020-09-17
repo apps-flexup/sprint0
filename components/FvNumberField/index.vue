44759516327b23bac084be44d3bca010e05e00da
gmevelec <gregory.mevelec@gmail.com>
2020-09-17 16:37:03 +0200

    Add number field component

---
+++ components/FvNumberField/index.vue
@@ -0,0 +1,86 @@
+<template lang="pug">
+.fv-number-field
+  fv-text-field(
+    class="right-input"
+    :value="inputValue"
+    :label="label"
+    :outlined="outlined"
+    :readonly="readonly"
+    :clearable="clearable"
+    :suffix="suffix"
+    @input="input"
+    @click:outside="onClickOutside"
+  )
+</template>
+
+<script>
+export default {
+  name: 'FvNumberField',
+  props: {
+    value: {
+      type: String,
+      default() {
+        return ''
+      }
+    },
+    label: {
+      type: String,
+      default() {
+        return ''
+      }
+    },
+    outlined: {
+      type: Boolean,
+      default() {
+        return true
+      }
+    },
+    readonly: {
+      type: Boolean,
+      default() {
+        return false
+      }
+    },
+    clearable: {
+      type: Boolean,
+      default() {
+        return true
+      }
+    },
+    suffix: {
+      type: String,
+      default() {
+        return ''
+      }
+    }
+  },
+  data() {
+    return {
+      inputValue: this.value
+    }
+  },
+  watch: {
+    value() {
+      this.inputValue = this.value
+    }
+  },
+  mounted() {
+    console.log('Composant', this.$options.name)
+  },
+  methods: {
+    input(v) {
+      this.inputValue = v
+      this.$emit('input', this.inputValue)
+    },
+    onClickOutside() {
+      this.$emit('click:outside')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.right-input >>> input {
+  text-align: right;
+}
+</style>
