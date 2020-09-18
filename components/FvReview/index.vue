c9a4b8f0ed3dad31628361cf8f068dbf6a7d7c94
gmevelec <gregory.mevelec@gmail.com>
2020-09-18 17:38:27 +0200

    Add FvReview component and story

---
+++ components/FvReview/index.vue
@@ -0,0 +1,95 @@
+<template lang="pug">
+.fv-review
+  v-row
+    v-col(cols="12" class="headDetails")
+      fv-avatar(
+        class="icon"
+        :avatar="avatar"
+      )
+      v-col(cols="10")
+        v-row
+          div.left {{ username }}
+          div {{ displayDate }}
+        v-row
+          v-rating(
+            :value="rating"
+            half-increments
+            :dense="true"
+            :readonly="true"
+            size="16"
+          )
+  v-row
+    div {{ review }}
+</template>
+
+<script>
+export default {
+  name: 'FvReview',
+  props: {
+    avatar: {
+      type: String,
+      default() {
+        return ''
+      }
+    },
+    username: {
+      type: String,
+      default() {
+        return ''
+      }
+    },
+    date: {
+      type: Date,
+      default() {
+        return null
+      }
+    },
+    review: {
+      type: String,
+      default() {
+        return ''
+      }
+    },
+    rating: {
+      type: Number,
+      default() {
+        return null
+      }
+    }
+  },
+  data() {
+    return {
+      displayDate: this.setDate(this.date)
+    }
+  },
+  watch: {
+    date() {
+      this.displayDate = this.setDate(this.date)
+    }
+  },
+  mounted() {
+    console.log('Composant', this.$options.name)
+  },
+  methods: {
+    setDate(date) {
+      if (!date) return null
+      const res = date.toLocaleString().substring(0, 10)
+      return res
+    }
+  }
+}
+</script>
+
+<style scoped>
+.icon {
+  padding-right: 5px;
+}
+.left {
+  text-align: right;
+  padding-right: 5px;
+}
+.headDetails {
+  display: flex;
+  align-items: center;
+}
+</style>
