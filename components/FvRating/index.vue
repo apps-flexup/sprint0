9f00faafb92b0dfba27c936334ed04b72f8311ff
gmevelec <gregory.mevelec@gmail.com>
2020-09-21 14:05:59 +0200

    Add Fv rating component and story

---
+++ components/FvRating/index.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+.fv-rating
+  v-rating(
+    :value="rating"
+    half-increments
+    :dense="dense"
+    :readonly="readonly"
+    size="16"
+    @input="input"
+  )
+</template>
+
+<script>
+export default {
+  name: 'FvReview',
+  props: {
+    rating: {
+      type: Number,
+      default() {
+        return 0
+      }
+    },
+    readonly: {
+      type: Boolean,
+      default() {
+        return true
+      }
+    },
+    dense: {
+      type: Boolean,
+      default() {
+        return false
+      }
+    }
+  },
+  mounted() {
+    console.log('Composant', this.$options.name)
+  },
+  methods: {
+    input(v) {
+      this.$emit('input', v)
+    }
+  }
+}
+</script>
