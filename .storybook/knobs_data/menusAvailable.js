df509131345fc9c28761b7351b64626216ac2e30
Antoine Puech <bitos@macbook-pro-de-antoine.home>
2020-10-28 17:48:29 +0100

    add knob title in FvSubMenu and add knob list in FvFindMenu to have a personalized list in storybook

--- .storybook/knobs_data/menusAvailable.js
+++ .storybook/knobs_data/menusAvailable.js
@@ -15,3 +15,42 @@ export const categories = {
 }
 
 export const defaultCategory = 'user' 
+
+export const fakeList = [
+    {
+        "active": true,
+        "icon": "mdi-circle",
+        "title": "titre test",
+        "to": "/"
+    },
+    {
+        "active": true,
+        "icon": "mdi-store-outline",
+        "title": "deuxieme titre",
+        "to": "/orders"
+    },
+    {
+        "active": true,
+        "icon": "mdi-briefcase-check",
+        "title": "manageMenu.contracts",
+        "to": "/contracts"
+    },
+    {
+        "active": true,
+        "icon": "mdi-flask-empty",
+        "title": "manageMenu.products",
+        "to": "/products"
+    },
+    {
+        "active": true,
+        "icon": "mdi-barcode",
+        "title": "manageMenu.offers",
+        "to": "/offers"
+    },
+    {
+        "active": true,
+        "icon": "mdi-email-outline",
+        "title": "manageMenu.contacts",
+        "to": "/contacts"
+    },
+]
