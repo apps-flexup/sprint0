0dc7204649702e7fa53195f95cd874b1559c044a
gmevelec <gregory.mevelec@gmail.com>
2020-10-30 16:03:57 +0100

    Allow to set settings when creating account

---
+++ pages/settings/new.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+.page-settings-new
+  v-card(
+    max-width="800px"
+    justify-center
+  )
+    v-card-title {{ $t('settings.title') }}
+    v-card-text
+      fv-settings-default
+    v-card-actions
+      v-spacer
+      v-btn(@click.stop="submit" block='') {{ $t('buttons.submit') }}
+</template>
+
+<script>
+export default {
+  methods: {
+    submit() {
+      this.$router.push('/products/new')
+    }
+  }
+}
+</script>
