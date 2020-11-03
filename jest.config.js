ae5d34422ed0c4bc43f3ccdb33cd78a442e25648
gmevelec <gregory.mevelec@gmail.com>
2020-11-03 17:17:16 +0100

    Remove coverage of untested files

--- jest.config.js
+++ jest.config.js
@@ -10,9 +10,9 @@ module.exports = {
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest'
   },
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
-  ]
+  collectCoverage: false
+  // collectCoverageFrom: [
+  //   '<rootDir>/components/**/*.vue',
+  //   '<rootDir>/pages/**/*.vue'
+  // ]
 }
