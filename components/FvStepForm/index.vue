bb23cb7a6761de807f768cbcec02193c60a327ca
Antoine Puech <bitos@macbook-pro-de-antoine.home>
2020-11-03 11:37:51 +0100

    create storie of FvStepForm, start creating FvProductForm in a v-list-group and create props for form's title and number

--- components/FvStepForm/index.vue
+++ components/FvStepForm/index.vue
@@ -1,13 +1,50 @@
 <template lang="pug">
 .fv-step-form
-
+  v-list
+    v-list-group(
+      :value='true'
+    )
+      template(v-slot:activator)
+        a.mr-4.number {{ formId }}
+        p.titleForm {{ title }}
+      fv-product-form
 </template>
 
 <script>
 export default {
   name: 'FvStepForm',
+  props: {
+    formId: {
+      type: Number,
+      default() {
+        return 1
+      }
+    },
+    title: {
+      type: String,
+      default() {
+        return 'Informations Générale'
+      }
+    }
+  },
   mounted() {
     console.log('Composant ', this.$options.name)
   }
 }
 </script>
+
+<style scoped>
+.titleForm {
+  margin: 0;
+  padding: 0;
+  font-size: 24px;
+  color: black;
+}
+.number {
+  border: black solid 1px;
+  padding: 0 8px;
+  border-radius: 50px;
+  color: black;
+  font-size: 18px;
+}
+</style>
