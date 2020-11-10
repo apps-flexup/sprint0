4357b896bcca8d31c28e105faf2bfdc576deeaf2
gmevelec <gregory.mevelec@gmail.com>
2020-11-10 17:19:14 +0100

    Add global component to create cards

---
+++ components/FvSlotItemCard/index.spec.js
@@ -0,0 +1,36 @@
+import { mount } from '@vue/test-utils'
+import FvSlotItemCard from './index.vue'
+
+describe('FvSlotItemCard', () => {
+  it('should render a slot item card', () => {
+    const wrapper = mount(FvSlotItemCard)
+    expect(wrapper.find('[data-testid="card"]').exists()).toBe(true)
+    expect(wrapper.find('[data-testid="cardDiv"]').exists()).toBe(true)
+    expect(wrapper.find('[data-testid="likeIcon"]').exists()).toBe(true)
+  })
+  it('should render default slot', () => {
+    const defaultSlot = 'helloWorld'
+    const wrapper = mount(FvSlotItemCard, {
+      slots: {
+        default: defaultSlot
+      }
+    })
+    expect(wrapper.html()).toContain(defaultSlot)
+  })
+  it('should emit an event when card clicked', () => {
+    const wrapper = mount(FvSlotItemCard)
+    const card = wrapper.find('[data-testid="cardDiv"]')
+    card.trigger('click')
+    const emitted = wrapper.emitted('card:clicked')
+    expect(emitted).toBeTruthy()
+    expect(emitted).toHaveLength(1)
+  })
+  it('should emit an event when like icon clicked', () => {
+    const wrapper = mount(FvSlotItemCard)
+    const likeIcon = wrapper.find('[data-testid="likeIcon"]')
+    likeIcon.vm.$emit('icon:clicked')
+    const emitted = wrapper.emitted('like:clicked')
+    expect(emitted).toBeTruthy()
+    expect(emitted).toHaveLength(1)
+  })
+})
---
+++ components/FvSlotItemCard/index.vue
@@ -0,0 +1,80 @@
+<template lang="pug">
+.fv-slot-item-card
+  v-card.rounded-lg(
+    data-testid="card"
+    :width="cardWidth"
+    :height="cardHeight"
+    :elevation="2"
+  )
+    div.clickCard(
+      data-testid="cardDiv"
+      @click="cardClicked"
+    )
+    fv-icon.like(
+      data-testid="likeIcon"
+      icon='mdi-heart-outline'
+      :color="likeColor"
+      size='large'
+      @icon:clicked='likeClicked'
+    )
+    slot(
+      data-testid="defaultSlot"
+    )
+</template>
+
+<script>
+export default {
+  name: 'FvSlotItemCard',
+  props: {
+    cardWidth: {
+      type: Number,
+      default() {
+        return 300
+      }
+    },
+    cardHeight: {
+      type: Number,
+      default() {
+        return 370
+      }
+    },
+    likeColor: {
+      type: String,
+      default() {
+        return 'black'
+      }
+    }
+  },
+  mounted() {
+    console.log('Composant ', this.$options.name)
+  },
+  methods: {
+    cardClicked() {
+      this.$emit('card:clicked')
+    },
+    likeClicked() {
+      this.$emit('like:clicked')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.v-card:hover {
+  transition: 0.5s;
+  box-shadow: 0px 3px 20px -3px !important;
+}
+.like {
+  position: absolute;
+  top: 2%;
+  right: 3%;
+  z-index: 2;
+}
+.clickCard {
+  position: absolute;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+}
+</style>
