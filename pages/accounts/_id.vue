719ca479ca65b28631eea7e366d526cf2fd561c1
gmevelec <gregory.mevelec@gmail.com>
2020-11-18 15:44:06 +0100

    Create account details page

--- pages/accounts/_id.vue
+++ pages/accounts/_id.vue
@@ -1,5 +1,20 @@
 <template lang="pug">
-.tmp
-  p coucou c'est les details
-  pre {{ this.$route.params }}
+.accounts-id
+  pre {{ account }}
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      accountId: this.$route.params.id
+    }
+  },
+  computed: {
+    account() {
+      const res = this.$store.getters['accounts/findById'](this.accountId)
+      return res
+    }
+  }
+}
+</script>
