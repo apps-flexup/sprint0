d088185ab383aed3ccabb1aa2248d156655e9ffa
gmevelec <gregory.mevelec@gmail.com>
2020-12-24 15:17:07 +0100

    Remove useless comment

--- store/offers/actions.js
+++ store/offers/actions.js
@@ -4,7 +4,6 @@ export default {
     // charger les contrat-types
     this.$repos.offers.indexWithAccountId().then((data) => {
       commit('set', data)
-      // commit('setHeaders', data.headers)
     })
   },
   getAll({ commit }) {
