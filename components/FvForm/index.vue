2a3ce13fbb7939905d90cd9859dd9b78bfbe56ac
gmevelec <gregory.mevelec@gmail.com>
2021-01-06 17:18:45 +0100

    Indent correctly fv form

--- components/FvForm/index.vue
+++ components/FvForm/index.vue
@@ -1,40 +1,40 @@
 <template lang="pug">
 .fv-form
-    v-row.head
-      fv-icon.mr-11(
-        data-testid="icon"
-        color="inherit"
-        size="xLarge"
-        icon="mdi-chevron-left"
-        @icon:clicked="cancel"
-      )
-      h1(data-testid="pageTitle") {{ $t('forms.' + form + '.' + action + '.title') }}
-    v-list.mt-10(
-      data-testid="listProductStep"
-      v-for="(step, index) in getFormStep"
-      :key="index"
+  v-row.head
+    fv-icon.mr-11(
+      data-testid="icon"
+      color="inherit"
+      size="xLarge"
+      icon="mdi-chevron-left"
+      @icon:clicked="cancel"
     )
-      fv-step-form(
-        data-testid="stepForm"
-        :formId="index+1"
-        :title="$t(step.title)"
-      )
-        template(slot="form")
-          composant(
-            data-testid="stepComponent"
-            :is="step.component"
-            :payload="payload"
-            @payload:changed="payloadChanged"
-          )
-    div.btn.mt-10
-      fv-secondary-button(
-        data-testid="cancelBtn"
-        @button:click="cancel"
-      ) {{ $t('forms.products.new.cancel') }}
-      fv-primary-button(
-        data-testid="submitBtn"
-        @button:click="submit"
-      ) {{ $t('forms.products.new.validate') }}
+    h1(data-testid="pageTitle") {{ $t('forms.' + form + '.' + action + '.title') }}
+  v-list.mt-10(
+    data-testid="listProductStep"
+    v-for="(step, index) in getFormStep"
+    :key="index"
+  )
+    fv-step-form(
+      data-testid="stepForm"
+      :formId="index+1"
+      :title="$t(step.title)"
+    )
+      template(slot="form")
+        composant(
+          data-testid="stepComponent"
+          :is="step.component"
+          :payload="payload"
+          @payload:changed="payloadChanged"
+        )
+  div.btn.mt-10
+    fv-secondary-button(
+      data-testid="cancelBtn"
+      @button:click="cancel"
+    ) {{ $t('forms.products.new.cancel') }}
+    fv-primary-button(
+      data-testid="submitBtn"
+      @button:click="submit"
+    ) {{ $t('forms.products.new.validate') }}
 </template>
 
 <script>
