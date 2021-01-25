5a33a740c4f693f00a6ef86ccfcaba8af14001d4
gmevelec <gregory.mevelec@gmail.com>
2021-01-25 15:56:38 +0100

    Fix price with unit sort

--- plugins/displayRules.js
+++ plugins/displayRules.js
@@ -63,11 +63,28 @@ const displayRules = (ctx) => ({
     return res
   },
   async priceWithUnit(item) {
-    const i18n = ctx.app.i18n
-    const formatedPrice = await this.priceToPreferredCurrency(item)
+    if (!item) return null
+    const price = item.price
+    if (!price) return null
+    const fromCurrency = item.currency
+    if (!fromCurrency) return null
+    const settings = ctx.store.getters['settings/settings']
+    const locale = ctx.store.getters['settings/locale']
+    const toCurrency = settings.currency
+    const minimumFractionDigits = settings.price_nb_after_decimal_point
+    const options = {
+      style: 'currency',
+      currency: toCurrency,
+      minimumFractionDigits
+    }
+    const convertedPrice = await convert(fromCurrency, toCurrency, price)
+    const formatedPrice = new Intl.NumberFormat(locale, options).format(
+      convertedPrice
+    )
     if (!formatedPrice) return null
     const unit = item.unit
     if (!unit) return null
+    const i18n = ctx.app.i18n
     const res = formatedPrice.toString() + '/' + i18n.t('units.symbol.' + unit)
     return res
   },
