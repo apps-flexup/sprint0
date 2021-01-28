220b762bd124785c5bc22e790a8fca16b7821449
gmevelec <gregory.mevelec@gmail.com>
2021-01-28 18:02:50 +0100

    Refactor order list

--- components/FvOrderList/index.vue
+++ components/FvOrderList/index.vue
@@ -1,28 +1,21 @@
 <template lang="pug">
 .fv-order-list
-  v-card
-    fv-data-table-header(
-      :title="$t('table.orders.title')"
-      :searchLabel="$t('table.orders.search')"
-      @dataTableSearch:changed="searchChanged"
-    )
-    fv-recursive-data-table(
-      :headers="mainHeaders"
-      :items="mainItems"
-      :filters="filters"
-      :rules="rules"
-      @dataTable:selected="orderSelected"
-    )
+  fv-index-table(
+    :title="$t('table.orders.title')"
+    :searchLabel="$t('table.orders.search')"
+    table="fv-recursive-data-table"
+    :headers="headers"
+    :items="items"
+    :rules="rules"
+    @list:selected="orderSelected"
+  )
 </template>
 
 <script>
-import { translateHeaders } from '~/plugins/utils'
-
 export default {
   name: 'FvOrderList',
   data() {
     return {
-      search: '',
       rules: {
         date: this.$displayRules.localeDate,
         amount: this.$displayRules.priceToPreferredCurrency,
@@ -32,33 +25,11 @@ export default {
     }
   },
   computed: {
-    filters() {
-      const res = []
-      if (this.search) res.push(this.search)
+    headers() {
+      const res = this.$activeAccount.headersOrders()
       return res
     },
-    mainHeaders() {
-      const headers = this.$activeAccount.headersOrders()
-      const res = translateHeaders(this.$i18n, headers.main)
-      return res
-    },
-    mainItems() {
-      const headers = this.$activeAccount.headersOrders()
-      if (!headers.sub) return []
-      const subHeaders = headers.sub
-      const subHeadersKeys = Object.keys(subHeaders)
-      subHeadersKeys.forEach((key) => {
-        subHeaders[key] = translateHeaders(this.$i18n, subHeaders[key])
-      })
-      const res = [
-        {
-          headers: subHeaders,
-          items: this.orderItems
-        }
-      ]
-      return res
-    },
-    orderItems() {
+    items() {
       const res = this.$activeAccount.orders()
       return res
     }
@@ -69,9 +40,6 @@ export default {
     this.$store.dispatch('orders/get')
   },
   methods: {
-    searchChanged(v) {
-      this.search = v
-    },
     orderSelected(order) {
       this.$emit('list:selected', order)
     }
