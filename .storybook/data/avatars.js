ea601f59904daab5a6059fc13cfa1246c4014aee
gmevelec <gregory.mevelec@gmail.com>
2021-02-16 16:16:15 +0100

    Add some stories

---
+++ .storybook/data/avatars.js
@@ -0,0 +1,10 @@
+export const avatarsAvailable = [
+  '/images/avatar-0.png',
+  '/images/avatar-1.png',
+  '/images/avatar-2.png',
+  '/images/avatar-3.png',
+  '/images/avatar-4.png',
+  '/images/avatar-5.png'
+]
+
+export const defaultAvatar = '/images/avatar-0.png'
