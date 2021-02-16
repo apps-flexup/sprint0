2d62553ef19736a88ebd7fcacbb79e60b53a0c85
gmevelec <gregory.mevelec@gmail.com>
2021-02-16 15:28:34 +0100

    Update some stories

---
+++ .storybook/data/menusAvailable.js
@@ -0,0 +1,61 @@
+export const menus = [
+  'user',
+  'account',
+  'seller',
+  'buyer',
+  'manage'
+]
+export const defaultMenu = 'user'
+
+export const categories = [
+  'user',
+  'account',
+  'space'
+]
+export const defaultCategory = 'user'
+
+export const spaceTitle = [
+  'buyer',
+  'seller',
+  'manage'
+]
+export const defaultSpaceTitle = 'manage'
+
+export const fakeList = [
+  {
+    "active": true,
+    "icon": "mdi-circle",
+    "title": "titre test",
+    "to": "/"
+  },
+  {
+    "active": true,
+    "icon": "mdi-store-outline",
+    "title": "deuxieme titre",
+    "to": "/orders"
+  },
+  {
+    "active": true,
+    "icon": "mdi-briefcase-check",
+    "title": "manageMenu.contracts",
+    "to": "/contracts"
+  },
+  {
+    "active": true,
+    "icon": "mdi-flask-empty",
+    "title": "manageMenu.products",
+    "to": "/products"
+  },
+  {
+    "active": true,
+    "icon": "mdi-barcode",
+    "title": "manageMenu.offers",
+    "to": "/offers"
+  },
+  {
+    "active": true,
+    "icon": "mdi-email-outline",
+    "title": "manageMenu.contacts",
+    "to": "/contacts"
+  },
+]
