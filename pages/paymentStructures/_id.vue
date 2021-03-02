b0cadfa498edc1bbcde7530eb81b63699ce1ac74
gmevelec <gregory.mevelec@gmail.com>
2021-03-02 15:55:57 +0100

    Add translation and remove

--- pages/paymentStructures/_id.vue
+++ pages/paymentStructures/_id.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
   fv-form(
-    :payload="getPaymentCondition"
-    @form:submit="editPaymentCondition"
+    :payload="getPaymentStructure"
+    @form:submit="editPaymentStructure"
     action='edit'
-    url='paymentConditions'
-    form='paymentConditions'
+    url='paymentStructures'
+    form='paymentStructures'
   )
 </template>
 
@@ -19,19 +19,19 @@ export default {
     }
   },
   computed: {
-    getPaymentCondition() {
-      const paymentConditionId = this.$route.params.id
-      const paymentCondition = this.$store.getters[
-        'paymentConditions/findById'
-      ](paymentConditionId)
-      const payload = JSON.parse(JSON.stringify(paymentCondition))
+    getPaymentStructure() {
+      const paymentStructureId = this.$route.params.id
+      const paymentStructure = this.$store.getters[
+        'paymentStructures/findById'
+      ](paymentStructureId)
+      const payload = JSON.parse(JSON.stringify(paymentStructure))
       return payload
     }
   },
   methods: {
-    editPaymentCondition(payload) {
+    editPaymentStructure(payload) {
       console.log('edit: ', payload)
-      this.$activeAccount.addPaymentCondition(payload)
+      this.$activeAccount.addPaymentStructure(payload)
     }
   }
 }
--- store/paymentStructures/actions.js
+++ store/paymentStructures/actions.js
@@ -3,6 +3,11 @@ export default {
   get({ commit }) {
     this.$repos.paymentStructures.index().then((data) => commit('set', data))
   },
+  remove({ commit }, paymentStructure) {
+    this.$repos.paymentStructures
+      .delete(paymentStructure.id)
+      .then(() => commit('remove', paymentStructure))
+  },
   add({ commit }, paymentStructure) {
     if (Object.prototype.hasOwnProperty.call(paymentStructure, 'id')) {
       this.$repos.paymentStructures.update(paymentStructure).then((res) => {
