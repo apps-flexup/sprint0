5c9d069d92178f0612253fd1fa6a42c130d52077
gmevelec <gregory.mevelec@gmail.com>
2021-03-04 16:17:46 +0100

    Connect to account before create story

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,9 @@ export * from '~~/.nuxt-storybook/storybook/preview.js'
 
 export const decorators = [(story) => ({
   components: { story },
+  beforeCreate() {
+    this.$store.commit('accounts/setCurrent', 1)
+  },
   template: '<v-app><story/></v-app>'
 })];
 
