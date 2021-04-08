edf43703827365f9e278e4a3e107d92e049100e1
Antoine Puech <bitos@macbook-pro-de-antoine.home>
2021-04-08 17:31:31 +0200

    add test of emit in FvVatField

--- components/FvVatField/index.spec.js
+++ components/FvVatField/index.spec.js
@@ -6,4 +6,14 @@ describe('FvVatField', () => {
     const wrapper = mount(FvVatField)
     expect(wrapper.find('[data-testid="vatField"]').exists()).toBe(true)
   })
+  it('should emit an event when vat change', () => {
+    const wrapper = mount(FvVatField)
+    const vatField = wrapper.find('[data-testid="vatField"]')
+    const vat = 20
+    vatField.vm.$emit('input', vat)
+    const submittedCalls = wrapper.emitted('vat:changed')
+    expect(submittedCalls).toBeTruthy()
+    expect(submittedCalls).toHaveLength(1)
+    expect(submittedCalls[0][0]).toBe(vat)
+  })
 })
