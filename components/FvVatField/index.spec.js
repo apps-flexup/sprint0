284d5de6d7786e38e636cc1a19291fc9e12a5809
Antoine Puech <bitos@macbook-pro-de-antoine.home>
2021-04-09 15:57:17 +0200

    test click outside of FvVatField

--- components/FvVatField/index.spec.js
+++ components/FvVatField/index.spec.js
@@ -2,12 +2,22 @@ import { mount } from '@vue/test-utils'
 import FvVatField from './index.vue'
 
 describe('FvVatField', () => {
+  let vat = 20
+  vat = (Math.round(vat * 100) / 100).toFixed(1)
+  const factory = (propsData) => {
+    return mount(FvVatField, {
+      propsData: {
+        ...propsData,
+        vat
+      }
+    })
+  }
   it('should render a vat field', () => {
-    const wrapper = mount(FvVatField)
+    const wrapper = factory()
     expect(wrapper.find('[data-testid="vatField"]').exists()).toBe(true)
   })
   it('should emit an event when vat change', () => {
-    const wrapper = mount(FvVatField)
+    const wrapper = factory()
     const vatField = wrapper.find('[data-testid="vatField"]')
     const vat = 20
     vatField.vm.$emit('input', vat)
@@ -15,5 +25,11 @@ describe('FvVatField', () => {
     expect(submittedCalls).toBeTruthy()
     expect(submittedCalls).toHaveLength(1)
     expect(submittedCalls[0][0]).toBe(vat)
+  })
+  it('should change vat when click outside', () => {
+    const wrapper = factory()
+    const vatField = wrapper.find('[data-testid="vatField"]')
+    vatField.vm.$emit('click:outside', vat)
+    expect(vat).toStrictEqual('20.0')
   })
 })
