5dc6dd8b329c7bf66c5c541d6136bac944f8ddda
gmevelec <gregory.mevelec@gmail.com>
2021-04-19 14:36:24 +0200

    Add tests for fv price with unit

--- components/FvPriceWithUnit/index.spec.js
+++ components/FvPriceWithUnit/index.spec.js
@@ -1,9 +1,34 @@
-import { mount } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
+import AsyncComputed from 'vue-async-computed'
 import FvPriceWithUnit from './index.vue'
+
+const localVue = createLocalVue()
+localVue.use(AsyncComputed)
+
+const $displayRules = {
+  priceWithUnit: () => 'priceWithUnit'
+}
+
+const factory = () => {
+  return mount(FvPriceWithUnit, {
+    localVue,
+    mocks: {
+      $displayRules
+    }
+  })
+}
 
 describe('FvPriceWithUnit', () => {
   it('should render a price with unit', () => {
-    const wrapper = mount(FvPriceWithUnit)
+    const wrapper = factory()
     expect(wrapper.find('[data-testid="price"]').exists()).toBe(true)
+  })
+  it('should display the result of display rule price with unit', async () => {
+    const wrapper = factory()
+    const price = wrapper.find('[data-testid="price"]')
+    // Ugly fix to wait enough help to FIXME
+    await wrapper.vm.$nextTick()
+    await wrapper.vm.$nextTick()
+    expect(price.text()).toBe('priceWithUnit')
   })
 })
