c1d75339a7a43d4a89d23b0bf8e84c502b1dd041
gmevelec <gregory.mevelec@gmail.com>
2021-07-08 19:57:18 +0200

    When creating an account, you're now admin and displayed in member list

--- store/members/getters.js
+++ store/members/getters.js
@@ -1,8 +1,7 @@
 export default {
   all(state) {
     if (!state.items) return []
-    const roles = JSON.parse(JSON.stringify(state.items))
-    const res = roles.filter((role) => role.role !== 'Owner')
+    const res = JSON.parse(JSON.stringify(state.items))
     return res
   }
 }
