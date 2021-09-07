3b39e67f3078ae27a9f487f4ab168ba596c6ec5a
gmevelec <gregory.mevelec@gmail.com>
2021-09-07 15:56:46 +0200

    tmp commit

---
+++ components/FvVisibilitySwitch/index.spec.js
@@ -0,0 +1,35 @@
+import { shallowMount } from '@vue/test-utils'
+import FvVisibilitySwitch from './index'
+
+const factory = (propsData) => {
+  return shallowMount(FvVisibilitySwitch, {
+    propsData: {
+      ...propsData
+    },
+    mocks: {
+      $t: (msg) => msg
+    }
+  })
+}
+
+describe('FvVisibilitySwitch', () => {
+  it('should render a fv visibility switch', () => {
+    const wrapper = factory()
+    expect(wrapper.find('[data-testid=switch]').exists()).toBe(true)
+  })
+  it.each([
+    ['private', 'public'],
+    ['public', 'private']
+  ])(
+    'should emit an event with %s when user click and visibility was %s',
+    (visibility, expectedVisibility) => {
+      const wrapper = factory({ value: visibility })
+      const switchBtn = wrapper.find('[data-testid=switch]')
+      switchBtn.vm.$emit('change')
+      const visibilityChange = wrapper.emitted('visibility:changed')
+      expect(visibilityChange).toBeTruthy()
+      expect(visibilityChange).toHaveLength(1)
+      expect(visibilityChange[0][0]).toBe(expectedVisibility)
+    }
+  )
+})
---
+++ components/FvVisibilitySwitch/index.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+.fv-visibility-switch
+  v-switch(
+    data-testid="switch"
+    :input-value="inputValue"
+    :label="`Visibility: ${value}`"
+    inset
+    @change="clicked"
+  )
+</template>
+
+<script>
+export default {
+  name: 'FvVisibilitySwitch',
+  props: {
+    value: {
+      type: String,
+      default() {
+        return 'public'
+      }
+    }
+  },
+  computed: {
+    inputValue() {
+      return this.value === 'public'
+    }
+  },
+  methods: {
+    clicked() {
+      let newVisibility
+      if (this.value === 'public') newVisibility = 'private'
+      else newVisibility = 'public'
+      this.$emit('visibility:changed', newVisibility)
+    }
+  }
+}
+</script>
