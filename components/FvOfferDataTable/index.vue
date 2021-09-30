3a88d851d90e19da184f031ab652944b07eedca4
gmevelec <gregory.mevelec@gmail.com>
2021-09-30 15:10:44 +0200

    Display price TTC in offer datatable

--- components/FvOfferDataTable/index.vue
+++ components/FvOfferDataTable/index.vue
@@ -15,6 +15,12 @@
         :currency="item.currency"
         :unit="item.unit"
       )
+    template(v-slot:item.price_ttc='{ item }')
+      fv-price-with-unit(
+        :price="priceWithTax(item)"
+        :currency="item.currency"
+        :unit="item.unit"
+      )
     template(v-slot:item.vat='{ item }')
       div {{ displayVat(item) }}
     template(v-slot:item.visibility='{ item }')
@@ -56,10 +62,16 @@ export default {
       }
     }
   },
-  mounted() {
-    console.log('Composant ', this.$options.name)
-  },
   methods: {
+    priceWithTax(item) {
+      const amountWithoutTax = item.price.amount
+      const vat = item.vat
+      const amountWithTax = amountWithoutTax + (amountWithoutTax * vat) / 100
+      return {
+        amount: amountWithTax,
+        currency: item.price.currency
+      }
+    },
     displayVat(item) {
       const res = this.$displayRules.vat(item)
       return res
