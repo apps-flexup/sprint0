879331d72c594471a2d2921810dac89fc6691c52
gmevelec <gregory.mevelec@gmail.com>
2021-10-07 13:33:43 +0200

    Refactor duplicated computed to a mixin

--- components/FvPriceFieldReadonly/index.vue
+++ components/FvPriceFieldReadonly/index.vue
@@ -10,9 +10,11 @@
 </template>
 
 <script>
-import { convert } from '~/plugins/currencies'
+import priceMixin from '~/mixins/price'
+
 export default {
   name: 'FvPriceFieldReadonly',
+  mixins: [priceMixin],
   props: {
     value: {
       type: Object,
@@ -33,45 +35,13 @@ export default {
       }
     }
   },
-  computed: {
-    suffix() {
-      const unit = this.search?.unit
-      let translatedUnit = ''
-      if (unit) translatedUnit = this.$t('units.symbol.' + unit.unit)
-      return `${this.preferredCurrency?.symbole}/${translatedUnit}`
-    },
-    preferredCurrency() {
-      const iso = this.$activeAccount.settings().currency
-      const res = this.$store.getters['currencies/findIso'](iso)
-      return res
-    },
-    nbDigitsAfterDecimalPoint() {
-      const settings = this.$activeAccount.settings()
-      if (!settings) return null
-      const res = settings.price_nb_after_decimal_point
-      return res
-    }
-  },
   asyncComputed: {
     async amount() {
-      const amount = this.value ? this.value.amount : null
-      if (!amount) return null
-      const fromCurrency = this.value.currency
-      if (!fromCurrency) return null
-      const toCurrency = this.preferredCurrency.iso3
-      if (!toCurrency) return null
-      let res = await convert(fromCurrency, toCurrency, amount)
-      if (this.nbDigitsAfterDecimalPoint) {
-        const multiplier = 10 ** this.nbDigitsAfterDecimalPoint
-        res = (Math.round(res * multiplier) / multiplier).toFixed(
-          this.nbDigitsAfterDecimalPoint
-        )
-      }
+      const amount = this.value?.amount
+      const fromCurrency = this.value?.currency
+      const res = await this.convertToPreferredCurrency(amount, fromCurrency)
       return res
     }
-  },
-  mounted() {
-    this.$store.dispatch('currencies/get')
   }
 }
 </script>
---
+++ mixins/price.js
@@ -0,0 +1,46 @@
+import { convert } from '~/plugins/currencies'
+
+export default {
+  computed: {
+    preferredCurrency() {
+      const iso = this.$activeAccount.settings().currency
+      const res = this.$store.getters['currencies/findIso'](iso)
+      return res
+    },
+    nbDigitsAfterDecimalPoint() {
+      const settings = this.$activeAccount.settings()
+      if (!settings) return null
+      const res = settings.price_nb_after_decimal_point
+      return res
+    },
+    suffix() {
+      const unit = this.search?.unit
+      let translatedUnit = ''
+      if (unit) translatedUnit = this.$t('units.symbol.' + unit.unit)
+      return `${this.preferredCurrency?.symbole}/${translatedUnit}`
+    }
+  },
+  mounted() {
+    this.$store.dispatch('currencies/get')
+  },
+  methods: {
+    async convertToPreferredCurrency(
+      amount,
+      fromCurrency,
+      truncatePrice = false
+    ) {
+      if (!amount) return null
+      if (!fromCurrency) return null
+      const toCurrency = this.preferredCurrency.iso3
+      if (!toCurrency) return null
+      let res = await convert(fromCurrency, toCurrency, amount)
+      if (truncatePrice && this.nbDigitsAfterDecimalPoint) {
+        const multiplier = 10 ** this.nbDigitsAfterDecimalPoint
+        res = (Math.round(res * multiplier) / multiplier).toFixed(
+          this.nbDigitsAfterDecimalPoint
+        )
+      }
+      return res
+    }
+  }
+}
