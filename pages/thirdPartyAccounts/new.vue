546e16782f850a698352513f3828c6cfc837b417
gmevelec <gregory.mevelec@gmail.com>
2021-11-10 16:17:07 +0100

    Design index page

--- pages/thirdPartyAccounts/new.vue
+++ pages/thirdPartyAccounts/new.vue
@@ -1,21 +1,22 @@
 <template lang="pug">
 .page.third-party-account-new
-  h3 Ajouter depuis l'annuaire flexup
-  fv-available-third-party-account-list
-  h3 Ajouter un tiers localement
-  fv-text-button(
-      data-testid="addNewLocalThirdParty"
-      @button:click="addNewLocalThirdParty"
-    )
-      template(v-slot:icon)
-        fv-icon(
-          size="small"
-          icon="mdi-plus"
-          color="#1976d2"
-          @icon:clicked="addNewLocalThirdParty"
-        )
-      template(v-slot:text)
-        | {{ $t('forms.thirdPartyAccounts.new.title') }}
+  v-col.mx-auto(cols="8")
+    h3.text-center.mb-5 Ajouter depuis l'annuaire flexup
+    fv-available-third-party-account-list
+    h3.text-center.mb-5 Ajouter un tiers localement
+    fv-text-button.justify-center(
+        data-testid="addNewLocalThirdParty"
+        @button:click="addNewLocalThirdParty"
+      )
+        template(v-slot:icon)
+          fv-icon(
+            size="small"
+            icon="mdi-plus"
+            color="#1976d2"
+            @icon:clicked="addNewLocalThirdParty"
+          )
+        template(v-slot:text)
+          | {{ $t('forms.thirdPartyAccounts.new.title') }}
 </template>
 
 <script>
