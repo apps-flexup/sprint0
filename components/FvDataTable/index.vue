3d023f53cd9c6c4ebe8a59da0ef9f49dae50582a
Antoine Puech <antoine.puech@edu.devinci.fr>
2021-11-24 11:10:38 +0100

    Style: add items per page options in fv datatable component

--- components/FvDataTable/index.vue
+++ components/FvDataTable/index.vue
@@ -8,7 +8,8 @@
     disable-sort
     :footer-props="{ \
       itemsPerPageText: $t('dataTable.footer.rowsPerPage'), \
-      itemsPerPageAllText: $t('dataTable.footer.all') \
+      itemsPerPageAllText: $t('dataTable.footer.all'), \
+      itemsPerPageOptions :[20, 50, 100, -1] \
     }"
     :server-items-length="itemsLength"
     :options="options"
