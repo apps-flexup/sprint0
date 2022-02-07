c1147cbc8e7ed088567d687db7ec830c90c30bbb
Frédéric LASNIER <jb@ouetquand.xyz>
2022-02-08 00:02:59 +0100

    fix: typo in component

--- components/FvRecursiveDataTable/index.vue
+++ components/FvRecursiveDataTable/index.vue
@@ -105,10 +105,9 @@ export default {
 ::v-deep .main-data-table > .v-data-table > .v-data-table__wrapper > table > thead {
   background-color: var(--mainTheadColor);
 }
-// TODO : Remove thos if really useless
-//::v-deep .v-data-table {
-//  border-radius: 0;
-//}
+::v-deep .v-data-table {
+  border-radius: 0;
+}
 ::v-deep .main-data-table > .v-data-table > .v-data-table__wrapper > table > tbody > tr > td {
   border-right: #0e0e0e0e solid 1px;
   padding: 0;
